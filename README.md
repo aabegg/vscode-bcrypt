53aa36d94dd724ca25e1b17c3356a4856189b7bf
aabegg <andreas.abegg@apmatic.ch>
2019-07-10 21:27:26 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# BCrypt
+
+Any string with any number of rounds can be converted to a hash with bcryptjs.
+
+## Features
+
+Convert string to hash
+
---
+++ src/extension.ts
@@ -0,0 +1,47 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as bcrypt from 'bcryptjs';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "bcyrpt" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.bcrypt', () => {
+		// The code you place here will be executed every time your command is executed
+
+		vscode.window.showInputBox({placeHolder: 'Enter password', prompt: 'Enter a password to hash with bcyrpt'}).then((password) => {
+			if (password) {
+
+				vscode.window.showInputBox({placeHolder: 'Enter salt', value: '10', prompt: 'How many Rounds should be used for hashing?'}).then((rounds) => {
+
+					if (rounds) {
+						const salt = bcrypt.genSaltSync(parseInt(rounds, 10));
+						const hash = bcrypt.hashSync(password, salt);
+						// Display a message box to the user
+						const actionItem = 'Copy to clipboard';
+						vscode.window.showInformationMessage(`${hash}`, actionItem).then((action) => {
+							if (actionItem === action) {
+								vscode.env.clipboard.writeText(hash);
+							}
+						});
+					}
+				});
+			}
+		});
+
+		
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
